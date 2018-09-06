649bae32ad50618a14bd29d20c716324437cf27e
alain <developer2.reliefapps@gmail.com>
2018-09-06 04:42:25 +0200

    maintenance

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+rm -rf dist/
+
 ng build sqlite-websql-orm --prod
 
-cp -R projects/sqlite-websql-orm/package.json ../../dist/sqlite-websql-orm/package.json
-cp readme.md dist/sqlite-websql-orm/readme.md
+cp -R projects/sqlite-websql-orm/package.json dist/sqlite-websql-orm/package.json
+cp README.md dist/sqlite-websql-orm/README.md
